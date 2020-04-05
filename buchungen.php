2c4c4489e4c51b9b4281974c13d484f6abf4a578
DamianMalik <github01@trendlinedata.com>
2020-04-05 12:49:57 +0200

    fix #2 Buchungen.php mit Datenbank verbunden.

--- buchungen.php
+++ buchungen.php
@@ -17,7 +17,7 @@
     <link rel="stylesheet" 
           href="style.css">
 
-    <title>Seitentitel</title>
+    <title>Buchungen</title>
 
     <!-- Google Fonts -->
     <!-- Font Type: Patrick Hand -->
@@ -29,30 +29,105 @@
 <body class="font-Bitter bg-light text-dark">
 
 	<?php
-	# <!-- Navigationsleiste -->
+	
+	# <!-- Navigationsleiste einfügen-->
 	include 'navigation.php';
+	
+	
+	# **********************************************************
+	# ***                   Definitionen                     ***
+	# ********************************************************** 
+	
+	# Werte aus der Formulareingabe "Bank" auslesen
+	# htmlentities --> Aus der Zeichenkette werden HTML-Tags in Code um. 
+	# strip_tags   --> Aus der Zeichenkette werden HTML- und PHP-Tags entfernt.
+	# preg_replace --> Aus der Zeichenkette werden bestimmte Zeichen gelöscht.
+	if(isset($_POST['Bank'])) {
+		$Bankid = preg_replace('![^0-9a-zA-ZäöüÄÖÜ\ ]!', '', strip_tags(htmlentities($_POST['Bank']))) ;
+	} else {
+		$Bankid = "X";
+	} // Ende der If-Abfrage
+
+
+
+	# Auslesen der Filtervariable 'Zeitraum'
+	if(isset($_GET['Zeitraum'])) {
+		$Zeitraum = preg_replace('![^0-9a-zA-ZäöüÄÖÜ\ ]!', '', strip_tags(htmlentities($_GET['Zeitraum']))) ;
+	} else {
+		$Zeitraum = "XX";
+	} // Ende der If-Abfrage
+
+
+
+	# Auslesen der Filtervariable 'Suchbegriff'
+	if(isset($_POST['Suchbegriff'])) {
+		$Suchbegriff = preg_replace('![^0-9a-zA-ZäöüÄÖÜ\ ]!', '', strip_tags(htmlentities($_POST['Suchbegriff']))) ;
+	} else {
+		$Suchbegriff = "";
+	} // Ende der If-Abfrage
+	# Für die LIKE-Bedingungen wird der Suchbegriff verfeinert
+	$Suchbegriff = "%" . $Suchbegriff . "%";
+
+
+	# **********************************************************
+	# ***                  Datenbankzugang                   ***
+	# **********************************************************
+	# Verbindungsaufbau zur Datenbank
+	include 'datenbank.php';
+	
+	
+	# Filter für Datenbankabfrage vordefinieren
+	if ($Bankid == '')   $Bankid = '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11'; 
+	if ($Bankid == 'X')  $Bankid = '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11'; 
+	if ($Zeitraum == '')   $Startdatum = "DATE_FORMAT(DATE_SUB(SYSDATE(), INTERVAL 20 YEAR), '%Y-%m-%d')"; 
+	if ($Zeitraum == 'XX') $Startdatum = "DATE_FORMAT(DATE_SUB(SYSDATE(), INTERVAL 20 YEAR), '%Y-%m-%d')"; 
+	if ($Zeitraum == '5J') $Startdatum = "DATE_FORMAT(DATE_SUB(SYSDATE(), INTERVAL 5 YEAR), '%Y-%m-%d')"; 
+	if ($Zeitraum == '3J') $Startdatum = "DATE_FORMAT(DATE_SUB(SYSDATE(), INTERVAL 3 YEAR), '%Y-%m-%d')"; 
+	if ($Zeitraum == '2J') $Startdatum = "DATE_FORMAT(DATE_SUB(SYSDATE(), INTERVAL 2 YEAR), '%Y-%m-%d')"; 
+	if ($Zeitraum == '1J') $Startdatum = "DATE_FORMAT(DATE_SUB(SYSDATE(), INTERVAL 1 YEAR), '%Y-%m-%d')"; 
+	if ($Zeitraum == '6M') $Startdatum = "DATE_FORMAT(DATE_SUB(SYSDATE(), INTERVAL 6 MONTH), '%Y-%m-%d')"; 
+	if ($Zeitraum == '3M') $Startdatum = "DATE_FORMAT(DATE_SUB(SYSDATE(), INTERVAL 3 MONTH), '%Y-%m-%d')"; 
+	
+	
+	# **********************************************************
+	# ***          Liste der Buchungen aus DB auslesen       ***
+	# **********************************************************
+
+	$DBabfrage = "SELECT CONCAT(b.`Bankname`, ' ', b.`Inhaber`, ' ', b.`Kontoname`) AS Bank,
+						a.`Buchungsdatum`, 
+						a.`Valuta`, 
+						a.`Buchungstyp`,
+						a.`AuftraggeberEmpfaenger`,
+						a.`Verwendungszweck`,
+						a.`Betrag`, 
+						a.`Waehrung`,
+						a.`Saldo`
+				FROM `Buchungen` a, 
+					`Banken` b
+				WHERE a.`BankID` = b.`id`
+				AND   a.`BankID` IN ( $Bankid )
+				AND   a.`Buchungsdatum` >=  " . $Startdatum . "
+				AND   ( a.`Verwendungszweck` LIKE '" 
+						. $Suchbegriff 
+						. "' OR    a.`AuftraggeberEmpfaenger` LIKE '" 
+						. $Suchbegriff . "' )
+				ORDER BY a.`Buchungsdatum` DESC, 
+						a.`Datensatznummer` DESC, 
+						a.`Datensatznummer` DESC; ";
+	$DBausgabe = $pdo->query($DBabfrage);
 	?>
-        <div class="container-fluid	">
-
+	<?php 
+	# **********************************************************
+	# ***                     Ausgabe                        ***
+	# **********************************************************
+	?>
+		<div class="container-fluid	">
             <h1 class="pt-3 pb-3">Buchungen</h1>
-
-            <!-- mb-3 etwas Abstand nach unten 
-            <div class="input-group mb-3">
-                <a href="#" class="btn btn-dark  active" role="button" aria-pressed="true">Alles</a>
-                <a href="#" class="btn btn-dark ml-1  active" role="button" aria-pressed="true">5 Jahre</a>
-                <a href="#" class="btn btn-dark ml-1 active" role="button" aria-pressed="true">3 Jahre</a>
-                <a href="#" class="btn btn-dark ml-1 active" role="button" aria-pressed="true">2 Jahre</a>
-                <a href="#" class="btn btn-dark ml-1 active" role="button" aria-pressed="true">1 Jahr</a>
-                <a href="#" class="btn btn-dark ml-1 active" role="button" aria-pressed="true">6 Monate</a>
-                <a href="#" class="btn btn-dark ml-1 active" role="button" aria-pressed="true">3 Monate</a>
-
-                <input type="text" class="form-control ml-2" placeholder="Suchbegriff" aria-label="Suche" aria-describedby="basic-addon1">
-                <div class="input-group-append">
-                    <span class="btn btn-dark" id="basic-addon2">Suche</span>
-                </div>
-            </div> -->
-<!---------------------------------------------------------------------->
-
+			<?php 
+			# **********************************************************
+			# ***               Filter-Buttons                       ***
+			# **********************************************************
+			?>
       
 			<div class="btn-group btn-group-lg pb-3">
 			<div class="btn-group btn-group-lg">
@@ -77,10 +152,8 @@
 			<a class="dropdown-item" href="#">2017</a>
 			</div>
 			</div>      
-		
-      
-
-
+			
+			
 			<input type="text" class="form-control form-control-lg ml-2" placeholder="Suchbegriff" aria-label="Suche" aria-describedby="basic-addon1">
 			<div class="input-group-append">
 				<span class="btn btn-dark btn-lg" id="basic-addon2">Suche</span>
@@ -88,12 +161,15 @@
 
 			</div>
 
+    
 
-<!----------------------------------------------------------->      
-
+            <?php 
+			# **********************************************************
+			# ***               Tabelle ausgeben                     ***
+			# **********************************************************
+			?>
             <!-- Responsive Tabelle mit Padding 3 -->
             <div class="table-responsive pt-3 h5">
-
                 <table class="table table-hover">
                     <thead class="thead-dark">
                         <tr>
@@ -106,34 +182,40 @@
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <th scope="row">B 1</th>
-                            <td>30.11.2019</td>
-                            <td>Überw</td>
-                            <td>Person XYZ</td>
-                            <td>Parkplatz</td>
-                            <td>-28,30</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">B 2</th>
-                            <td>05.12.2019</td>
-                            <td>Auftraggeber</td>
-                            <td>Carwash</td>
-                            <td>Auftraggeber</td>
-                            <td>-13,20</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">b 3</th>
-                            <td>07.12.2019</td>
-                            <td>Überw</td>
-                            <td>Firma XYZ</td>
-                            <td>Sitzkissen</td>
-                            <td>-150,00</td>
-                        </tr>
+					<?php
+					foreach($DBausgabe as $zeile) { 
+						echo '<tr>';
+							echo '<td scope="row">';
+								echo $zeile['Bank'];
+							echo '</td>';
+                            echo '<td>'; 
+								echo date('d.m.Y', strtotime($zeile['Buchungsdatum'])); 
+							echo '</td>';
+							echo '<td>';
+								echo $zeile['Buchungstyp'];
+							echo '</td>';
+                            echo '<td>';
+								echo $zeile['AuftraggeberEmpfaenger'];
+							echo '</td>';
+                            echo '<td>';
+								echo $zeile['Verwendungszweck'];
+							echo '</td>';
+							echo '<td>';
+								if ($zeile['Betrag'] > 0) $farbe = '#00B233'; // grüne Farbe
+								if ($zeile['Betrag'] < 0) $farbe = '#FF0000'; // rote Farbe
+								echo "<font color='" . $farbe . "'>" 
+									 .number_format($zeile['Betrag'], 2, ",", ".")
+									 . "</font>";
+							echo '</td>';
+                        echo '</tr>';
+                    
+					}
+					?>
                     </tbody>
+                    
                 </table>
-
             </div>
+
 
             <!-- Optional JavaScript -->
             <!-- jQuery first, then Popper.js, then Bootstrap JS -->
